9f2b8466e2573fef4603bb884d9b5e8e342d39c1
Omer Ada <50885089+adagames@users.noreply.github.com>
2025-02-05 22:04:15 +0300

    git workflow.

--- api-gateway/src/main/java/com/craftpilot/apigateway/config/FirestoreConfig.java
+++ api-gateway/src/main/java/com/craftpilot/apigateway/config/FirestoreConfig.java
@@ -2,35 +2,30 @@ package com.craftpilot.apigateway.config;
 
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.cloud.firestore.Firestore;
-import com.google.firebase.FirebaseApp;
-import com.google.firebase.FirebaseOptions;
-import com.google.firebase.cloud.FirestoreClient;
+import com.google.cloud.firestore.FirestoreOptions;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ClassPathResource;
 
+import java.io.FileInputStream;
 import java.io.IOException;
 
 @Configuration
 public class FirestoreConfig {
 
-    @Value("${spring.cloud.gcp.project-id}")
-    private String projectId;
+    @Value("${GOOGLE_APPLICATION_CREDENTIALS:/gcp-credentials.json}")
+    private String credentialsPath;
 
     @Bean
     public Firestore firestore() throws IOException {
-        ClassPathResource serviceAccount = new ClassPathResource("firebase-service-account.json");
+        GoogleCredentials credentials = GoogleCredentials.fromStream(
+            new FileInputStream(credentialsPath)
+        );
 
-        FirebaseOptions options = FirebaseOptions.builder()
-                .setCredentials(GoogleCredentials.fromStream(serviceAccount.getInputStream()))
-                .setProjectId(projectId)
-                .build();
+        FirestoreOptions firestoreOptions = FirestoreOptions.newBuilder()
+            .setCredentials(credentials)
+            .build();
 
-        if (FirebaseApp.getApps().isEmpty()) {
-            FirebaseApp.initializeApp(options);
-        }
-
-        return FirestoreClient.getFirestore();
+        return firestoreOptions.getService();
     }
 }
