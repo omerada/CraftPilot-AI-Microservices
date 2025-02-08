7b9530e0d6873f91c0bb993a8e82ed65da51e1fc
Omer Ada <50885089+adagames@users.noreply.github.com>
2025-02-08 21:09:43 +0300

    git workflow

--- notification-service/src/main/java/com/craftpilot/notificationservice/config/FirebaseConfig.java
+++ notification-service/src/main/java/com/craftpilot/notificationservice/config/FirebaseConfig.java
@@ -4,28 +4,28 @@ import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import com.google.firebase.messaging.FirebaseMessaging;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ClassPathResource;
 
+import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStream;
 
 @Configuration
 public class FirebaseConfig {
 
+    @Value("${GOOGLE_APPLICATION_CREDENTIALS}")
+    private String credentialsPath;
+
     @Bean
     public FirebaseMessaging firebaseMessaging() throws IOException {
         if (FirebaseApp.getApps().isEmpty()) {
-            InputStream serviceAccount = new ClassPathResource("firebase-service-account.json").getInputStream();
-            
+            FileInputStream serviceAccount = new FileInputStream(credentialsPath);
             FirebaseOptions options = FirebaseOptions.builder()
-                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
-                    .build();
-
+                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                .build();
             FirebaseApp.initializeApp(options);
         }
-        
         return FirebaseMessaging.getInstance();
     }
 }
