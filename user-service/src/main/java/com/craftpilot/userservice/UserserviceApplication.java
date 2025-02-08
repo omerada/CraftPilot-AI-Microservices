a4b4cb4cbc886915c602e5aa46ffcb49a8b21146
Omer Ada <50885089+adagames@users.noreply.github.com>
2025-02-08 16:06:00 +0300

    git workflow.

--- user-service/src/main/java/com/craftpilot/userservice/UserserviceApplication.java
+++ user-service/src/main/java/com/craftpilot/userservice/UserserviceApplication.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class UserServiceApplication {
+public class UserserviceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(UserServiceApplication.class, args);
+        SpringApplication.run(UserserviceApplication.class, args);
     }
 
 }
