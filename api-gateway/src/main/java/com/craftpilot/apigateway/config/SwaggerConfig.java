db58af46e70adb952e21ffa8a7d4681959a8cbb9
Omer Ada <50885089+adagames@users.noreply.github.com>
2025-02-09 12:45:40 +0300

    git workflow

--- api-gateway/src/main/java/com/craftpilot/apigateway/config/SwaggerConfig.java
+++ api-gateway/src/main/java/com/craftpilot/apigateway/config/SwaggerConfig.java
@@ -1,19 +1,50 @@
 package com.craftpilot.apigateway.config;
 
-import org.springframework.cloud.gateway.route.RouteLocator;
-import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.gateway.route.RouteDefinition;
+import org.springframework.cloud.gateway.route.RouteDefinitionLocator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import reactor.core.publisher.Flux;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
-    
+
+    @Value("${spring.application.name}")
+    private String applicationName;
+
     @Bean
-    public RouteLocator swaggerRouteLocator(RouteLocatorBuilder builder) {
-        return builder.routes()
-                .route("swagger_route", r -> r
-                        .path("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html")
-                        .uri("http://localhost:8080"))
-                .build();
+    public OpenAPI apiInfo() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("CraftPilot API Gateway")
+                        .description("API Gateway for CraftPilot Platform")
+                        .version("1.0.0"));
+    }
+
+    @Bean
+    @Primary
+    public List<GroupedOpenApi> apis(RouteDefinitionLocator locator) {
+        List<GroupedOpenApi> groups = new ArrayList<>();
+        Flux<RouteDefinition> definitions = locator.getRouteDefinitions();
+        
+        definitions
+                .filter(routeDefinition -> routeDefinition.getId().matches(".*-service"))
+                .subscribe(routeDefinition -> {
+                    String name = routeDefinition.getId().replaceAll("-service", "");
+                    groups.add(GroupedOpenApi.builder()
+                            .pathsToMatch("/" + name + "/**")
+                            .group(name)
+                            .build());
+                });
+
+        return groups;
     }
 }
