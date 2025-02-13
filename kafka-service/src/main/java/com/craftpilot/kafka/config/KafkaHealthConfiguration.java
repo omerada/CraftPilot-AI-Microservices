ef9597d776d9d094a016c9c24c9ed4bd84fbb15e
Omer Ada <50885089+adagames@users.noreply.github.com>
2025-02-13 21:07:59 +0300

    git workflow.

--- kafka-service/src/main/java/com/craftpilot/kafka/config/KafkaHealthConfiguration.java
+++ kafka-service/src/main/java/com/craftpilot/kafka/config/KafkaHealthConfiguration.java
@@ -1,10 +1,11 @@
 package com.craftpilot.kafka.config;
 
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthContributor;
+import org.springframework.boot.actuate.health.HealthIndicator;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.boot.actuate.health.HealthContributor;
-import org.springframework.boot.actuate.health.HealthIndicator;
 
 @Configuration
 public class KafkaHealthConfiguration {
@@ -12,8 +13,6 @@ public class KafkaHealthConfiguration {
     @Bean
     @ConditionalOnMissingBean(name = "kafka")
     public HealthContributor kafkaHealthContributor() {
-        return (HealthIndicator) () -> {
-            return Health.up().build();
-        }
+        return (HealthIndicator) () -> Health.up().build();
     }
 }
