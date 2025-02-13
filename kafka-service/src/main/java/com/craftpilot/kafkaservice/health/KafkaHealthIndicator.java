7f042892e764d42d4abe4e3917b06550f768ecda
Omer Ada <50885089+adagames@users.noreply.github.com>
2025-02-13 23:18:50 +0300

    git workflow.

---
+++ kafka-service/src/main/java/com/craftpilot/kafkaservice/health/KafkaHealthIndicator.java
@@ -0,0 +1,36 @@
+package com.craftpilot.kafkaservice.health;
+
+import org.apache.kafka.clients.admin.AdminClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.kafka.core.KafkaAdmin;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.TimeUnit;
+
+@Component("kafka")
+public class KafkaHealthIndicator implements HealthIndicator {
+
+    private final KafkaAdmin kafkaAdmin;
+
+    @Autowired
+    public KafkaHealthIndicator(KafkaAdmin kafkaAdmin) {
+        this.kafkaAdmin = kafkaAdmin;
+    }
+
+    @Override
+    public Health health() {
+        try (AdminClient adminClient = AdminClient.create(kafkaAdmin.getConfigurationProperties())) {
+            adminClient.listTopics().names().get(5, TimeUnit.SECONDS);
+            return Health.up()
+                    .withDetail("kafka", "Kafka broker is accessible")
+                    .build();
+        } catch (Exception e) {
+            return Health.down()
+                    .withDetail("kafka", "Kafka broker is not accessible")
+                    .withException(e)
+                    .build();
+        }
+    }
+}
