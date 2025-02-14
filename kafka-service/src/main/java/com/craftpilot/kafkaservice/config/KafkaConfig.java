e030b11908eb100248b0ed83ceb861490405a937
Omer Ada <50885089+adagames@users.noreply.github.com>
2025-02-15 00:45:42 +0300

    git workflow.

--- kafka-service/src/main/java/com/craftpilot/kafkaservice/config/KafkaConfig.java
+++ kafka-service/src/main/java/com/craftpilot/kafkaservice/config/KafkaConfig.java
@@ -3,6 +3,8 @@ package com.craftpilot.kafkaservice.config;
 import lombok.RequiredArgsConstructor;
 import org.apache.kafka.clients.admin.AdminClientConfig;
 import org.apache.kafka.clients.admin.NewTopic;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.actuate.autoconfigure.health.ConditionalOnEnabledHealthIndicator;
 import org.springframework.boot.actuate.autoconfigure.health.HealthContributorAutoConfiguration;
 import org.springframework.boot.context.properties.ConfigurationProperties;
@@ -19,20 +21,24 @@ import java.util.Map;
 @Configuration
 @EnableKafka
 @Import(HealthContributorAutoConfiguration.class)
-@ConditionalOnEnabledHealthIndicator("health")
+@ConditionalOnEnabledHealthIndicator("kafka")
 @RequiredArgsConstructor
 public class KafkaConfig {
+
+    @Value("${spring.kafka.bootstrap-servers}")
+    private String bootstrapServers;
 
     private final KafkaTopicsProperties topicsProperties;
 
     @Bean
     public KafkaAdmin kafkaAdmin() {
         Map<String, Object> configs = new HashMap<>();
-        configs.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, 
-                   topicsProperties.getBootstrapServers());
+        configs.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         configs.put(AdminClientConfig.RETRIES_CONFIG, 5);
         configs.put(AdminClientConfig.RETRY_BACKOFF_MS_CONFIG, 1000);
         configs.put(AdminClientConfig.REQUEST_TIMEOUT_MS_CONFIG, 30000);
+        configs.put(AdminClientConfig.DEFAULT_API_TIMEOUT_MS_CONFIG, "30000");
+        configs.put(ProducerConfig.RECONNECT_BACKOFF_MS_CONFIG, "1000");
         return new KafkaAdmin(configs);
     }
 
