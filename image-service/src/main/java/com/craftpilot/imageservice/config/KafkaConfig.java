ee39d1e15576f111ca6750081fd6b921fcfbc092
Omer Ada <50885089+adagames@users.noreply.github.com>
2025-02-15 20:28:43 +0300

    git workflow.

--- image-service/src/main/java/com/craftpilot/imageservice/config/KafkaConfig.java
+++ image-service/src/main/java/com/craftpilot/imageservice/config/KafkaConfig.java
@@ -2,6 +2,7 @@ package com.craftpilot.imageservice.config;
 
 import com.craftpilot.imageservice.config.KafkaBaseConfig;
 import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.TopicBuilder;
@@ -9,13 +10,11 @@ import org.springframework.kafka.config.TopicBuilder;
 @Configuration
 public class KafkaConfig extends KafkaBaseConfig {
 
-    @Bean
-    public NewTopic imageEventsTopic() {
-        return createTopic("image-events");
-    }
+    @Value("${kafka.topics.image-events}")
+    private String imageEventsTopic;
 
     @Bean
-    public NewTopic imageGenerationEventsTopic() {
-        return createTopic("image-generation-events");
+    public NewTopic imageEventsTopic() {
+        return createTopic(imageEventsTopic);
     }
 }
