420a67dfd2ae82cfe8164fabc875f36bee6d63e9
Omer Ada <50885089+adagames@users.noreply.github.com>
2025-03-01 15:36:57 +0300

    fix

--- llm-service/src/main/java/com/craftpilot/llmservice/config/CircuitBreakerConfiguration.java
+++ llm-service/src/main/java/com/craftpilot/llmservice/config/CircuitBreakerConfiguration.java
@@ -1,51 +1,56 @@
 package com.craftpilot.llmservice.config;
 
 import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
+import io.github.resilience4j.circuitbreaker.CircuitBreakerRegistry;
 import io.github.resilience4j.timelimiter.TimeLimiterConfig;
+import io.github.resilience4j.timelimiter.TimeLimiterRegistry;
 import org.springframework.cloud.circuitbreaker.resilience4j.ReactiveResilience4JCircuitBreakerFactory;
 import org.springframework.cloud.client.circuitbreaker.ReactiveCircuitBreaker;
 import org.springframework.cloud.client.circuitbreaker.ReactiveCircuitBreakerFactory;
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JConfigBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
 import java.time.Duration;
-import java.util.function.Consumer;
 
 @Configuration
 public class CircuitBreakerConfiguration {
 
     @Bean
-    public ReactiveCircuitBreakerFactory reactiveCircuitBreakerFactory() {
-        ReactiveResilience4JCircuitBreakerFactory factory = new ReactiveResilience4JCircuitBreakerFactory();
-        
-        factory.configureDefault(id -> new io.github.resilience4j.circuitbreaker.CircuitBreakerConfig.Builder()
-            .slidingWindowSize(10)
-            .failureRateThreshold(50)
-            .waitDurationInOpenState(Duration.ofSeconds(10))
-            .permittedNumberOfCallsInHalfOpenState(5)
-            .build());
+    public CircuitBreakerRegistry circuitBreakerRegistry() {
+        return CircuitBreakerRegistry.ofDefaults();
+    }
+
+    @Bean
+    public TimeLimiterRegistry timeLimiterRegistry() {
+        return TimeLimiterRegistry.ofDefaults();
+    }
+
+    @Bean
+    public ReactiveResilience4JCircuitBreakerFactory reactiveCircuitBreakerFactory(
+            CircuitBreakerRegistry circuitBreakerRegistry,
+            TimeLimiterRegistry timeLimiterRegistry) {
             
+        ReactiveResilience4JCircuitBreakerFactory factory = 
+            new ReactiveResilience4JCircuitBreakerFactory(circuitBreakerRegistry, timeLimiterRegistry);
+
+        factory.configureDefault(id -> new Resilience4JConfigBuilder(id)
+            .circuitBreakerConfig(CircuitBreakerConfig.custom()
+                .slidingWindowSize(10)
+                .failureRateThreshold(50)
+                .waitDurationInOpenState(Duration.ofSeconds(10))
+                .permittedNumberOfCallsInHalfOpenState(5)
+                .build())
+            .timeLimiterConfig(TimeLimiterConfig.custom()
+                .timeoutDuration(Duration.ofSeconds(30))
+                .build())
+            .build());
+
         return factory;
     }
 
     @Bean
     public ReactiveCircuitBreaker openRouterCircuitBreaker(ReactiveCircuitBreakerFactory circuitBreakerFactory) {
         return circuitBreakerFactory.create("openRouter");
-    }
-
-    @Bean
-    public Consumer<ReactiveResilience4JCircuitBreakerFactory> openRouterCustomizer() {
-        return factory -> factory.configure(
-            builder -> builder
-                .circuitBreakerConfig(CircuitBreakerConfig.custom()
-                    .slidingWindowSize(10)
-                    .failureRateThreshold(50)
-                    .waitDurationInOpenState(Duration.ofSeconds(10))
-                    .permittedNumberOfCallsInHalfOpenState(5)
-                    .build())
-                .timeLimiterConfig(TimeLimiterConfig.custom()
-                    .timeoutDuration(Duration.ofSeconds(30))
-                    .build()),
-            "openRouter"
-        );
     }
 }
