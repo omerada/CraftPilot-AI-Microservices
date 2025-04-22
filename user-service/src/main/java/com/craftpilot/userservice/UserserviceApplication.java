ca68083ae133139c1a042e0d927a81ee943b9895
Omer Ada <50885089+adagames@users.noreply.github.com>
2025-04-22 21:30:54 +0300

    user prefences güncelleme yapıldı.

--- user-service/src/main/java/com/craftpilot/userservice/UserserviceApplication.java
+++ user-service/src/main/java/com/craftpilot/userservice/UserserviceApplication.java
@@ -2,6 +2,7 @@ package com.craftpilot.userservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import com.craftpilot.userservice.config.LightSecurityConfig;
 import org.springframework.context.annotation.Import;
@@ -9,6 +10,7 @@ import org.springframework.context.annotation.Import;
 @Import(LightSecurityConfig.class)
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableReactiveMongoRepositories(basePackages = "com.craftpilot.userservice.repository")
 public class UserserviceApplication {
 
     public static void main(String[] args) {
