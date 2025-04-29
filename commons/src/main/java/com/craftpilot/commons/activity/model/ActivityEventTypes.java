4774b747a4836ecf8ca49be5a26bc5752bf3acf4
Omer Ada <50885089+adagames@users.noreply.github.com>
2025-04-30 00:26:25 +0300

    ActivityEventTypes sınıfındaki gereksiz yorum satırı kaldırıldı.

--- commons/src/main/java/com/craftpilot/commons/activity/model/ActivityEventTypes.java
+++ commons/src/main/java/com/craftpilot/commons/activity/model/ActivityEventTypes.java
@@ -4,8 +4,6 @@ public class ActivityEventTypes {
 
     public static final String CHAT_HISTORY_ARCHIVE = "CHAT_HISTORY_ARCHIVE";
     public static final String CHAT_HISTORY_UNARCHIVE = "CHAT_HISTORY_UNARCHIVE";
-    
-    // Other existing constants for different activities
     public static final String CHAT_HISTORY_CREATE = "CHAT_HISTORY_CREATE";
     public static final String CHAT_HISTORY_UPDATE = "CHAT_HISTORY_UPDATE";
     public static final String CHAT_HISTORY_DELETE = "CHAT_HISTORY_DELETE";
