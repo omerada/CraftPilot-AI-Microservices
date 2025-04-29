dce5ea1205e141a3a1d97c40c6ef9eaf3ac19347
Omer Ada <50885089+adagames@users.noreply.github.com>
2025-04-30 00:38:20 +0300

    ActivityEventTypes sınıfına sohbet geçmişi arşivleme işlemleri için sabit eklendi.

--- craft-pilot-commons/src/main/java/com/craftpilot/commons/activity/model/ActivityEventTypes.java
+++ craft-pilot-commons/src/main/java/com/craftpilot/commons/activity/model/ActivityEventTypes.java
@@ -12,6 +12,7 @@ public final class ActivityEventTypes {
     public static final String CHAT_HISTORY_UPDATE = "CHAT_HISTORY_UPDATE";
     public static final String CHAT_HISTORY_DELETE = "CHAT_HISTORY_DELETE";
     public static final String CHAT_HISTORY_ARCHIVE = "CHAT_HISTORY_ARCHIVE";
+    public static final String CHAT_HISTORY_UNARCHIVE = "CHAT_HISTORY_UNARCHIVE";
     public static final String CONVERSATION_CREATE = "CONVERSATION_CREATE";
     public static final String TITLE_UPDATE = "TITLE_UPDATE";
     
