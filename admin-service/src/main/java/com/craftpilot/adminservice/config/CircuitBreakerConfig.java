b41d91d0169e56c59b588a48172bbd1dbc3cecc6
o-oada <adasunucum06@gmail.com>
2025-05-13 17:22:46 +0300

    feat: MongoDB yapılandırması ve sağlık kontrolleri güncellendi; bağlantı ayarları, zaman aşımı değerleri ve hata yönetimi iyileştirildi. CI/CD sürecine MongoDB konteyner kontrolü eklendi.

--- admin-service/src/main/java/com/craftpilot/adminservice/config/CircuitBreakerConfig.java
+++ admin-service/src/main/java/com/craftpilot/adminservice/config/CircuitBreakerConfig.java
@@ -14,87 +14,93 @@ import java.time.Duration;
 @Configuration
 public class CircuitBreakerConfig {
 
-    @Bean
-    public CircuitBreakerRegistry circuitBreakerRegistry() {
-        return CircuitBreakerRegistry.ofDefaults();
-    }
+        @Bean
+        public CircuitBreakerRegistry circuitBreakerRegistry() {
+                return CircuitBreakerRegistry.ofDefaults();
+        }
 
-    @Bean
-    public Customizer<ReactiveResilience4JCircuitBreakerFactory> adminServiceCustomizer() {
-        return factory -> {
-            factory.configure(builder -> builder
-                    .circuitBreakerConfig(io.github.resilience4j.circuitbreaker.CircuitBreakerConfig.custom()
-                            .slidingWindowType(SlidingWindowType.COUNT_BASED)
-                            .slidingWindowSize(20)
-                            .failureRateThreshold(40)
-                            .waitDurationInOpenState(Duration.ofSeconds(30))
-                            .permittedNumberOfCallsInHalfOpenState(5)
-                            .build())
-                    .timeLimiterConfig(TimeLimiterConfig.custom()
-                            .timeoutDuration(Duration.ofSeconds(5))
-                            .build()),
-                    "adminService");
-        };
-    }
+        @Bean
+        public Customizer<ReactiveResilience4JCircuitBreakerFactory> adminServiceCustomizer() {
+                return factory -> {
+                        factory.configure(builder -> builder
+                                        .circuitBreakerConfig(io.github.resilience4j.circuitbreaker.CircuitBreakerConfig
+                                                        .custom()
+                                                        .slidingWindowType(SlidingWindowType.COUNT_BASED)
+                                                        .slidingWindowSize(20)
+                                                        .failureRateThreshold(40)
+                                                        .waitDurationInOpenState(Duration.ofSeconds(30))
+                                                        .permittedNumberOfCallsInHalfOpenState(5)
+                                                        .build())
+                                        .timeLimiterConfig(TimeLimiterConfig.custom()
+                                                        .timeoutDuration(Duration.ofSeconds(5))
+                                                        .build()),
+                                        "adminService");
+                };
+        }
 
-    @Bean
-    public Customizer<ReactiveResilience4JCircuitBreakerFactory> mongoServiceCustomizer() {
-        return factory -> {
-            factory.configure(builder -> builder
-                    .circuitBreakerConfig(io.github.resilience4j.circuitbreaker.CircuitBreakerConfig.custom()
-                            .slidingWindowType(SlidingWindowType.COUNT_BASED)
-                            .slidingWindowSize(15)
-                            .failureRateThreshold(40)
-                            .waitDurationInOpenState(Duration.ofSeconds(10))
-                            .permittedNumberOfCallsInHalfOpenState(3)
-                            .automaticTransitionFromOpenToHalfOpenEnabled(true)
-                            .recordExceptions(
-    com.mongodb.MongoTimeoutException.class,
-    com.mongodb.MongoSocketException.class,
-    com.mongodb.MongoExecutionTimeoutException.class,
-    com.mongodb.MongoQueryException.class,
-    com.mongodb.MongoWriteException.class)
-                            .build())
-                    .timeLimiterConfig(TimeLimiterConfig.custom()
-                            .timeoutDuration(Duration.ofSeconds(3))
-                            .build()),
-                    "mongoService");
-        };
-    }
+        @Bean
+        public Customizer<ReactiveResilience4JCircuitBreakerFactory> mongoServiceCustomizer() {
+                return factory -> {
+                        factory.configure(builder -> builder
+                                        .circuitBreakerConfig(io.github.resilience4j.circuitbreaker.CircuitBreakerConfig
+                                                        .custom()
+                                                        .slidingWindowType(SlidingWindowType.COUNT_BASED)
+                                                        .slidingWindowSize(15)
+                                                        .failureRateThreshold(40)
+                                                        .waitDurationInOpenState(Duration.ofSeconds(30)) // 10 -> 30
+                                                        .permittedNumberOfCallsInHalfOpenState(3)
+                                                        .automaticTransitionFromOpenToHalfOpenEnabled(true)
+                                                        .recordExceptions(
+                                                                        com.mongodb.MongoTimeoutException.class,
+                                                                        com.mongodb.MongoSocketException.class,
+                                                                        com.mongodb.MongoExecutionTimeoutException.class,
+                                                                        com.mongodb.MongoQueryException.class,
+                                                                        com.mongodb.MongoWriteException.class,
+                                                                        com.mongodb.MongoCommandException.class,
+                                                                        java.net.UnknownHostException.class)
+                                                        .build())
+                                        .timeLimiterConfig(TimeLimiterConfig.custom()
+                                                        .timeoutDuration(Duration.ofSeconds(30)) // 3 -> 30
+                                                        .build()),
+                                        "mongoService");
+                };
+        }
 
-    @Bean
-    public Customizer<ReactiveResilience4JCircuitBreakerFactory> redisServiceCustomizer() {
-        return factory -> {
-            factory.configure(builder -> builder
-                    .circuitBreakerConfig(io.github.resilience4j.circuitbreaker.CircuitBreakerConfig.custom()
-                            .slidingWindowType(SlidingWindowType.COUNT_BASED)
-                            .slidingWindowSize(15)
-                            .failureRateThreshold(45)
-                            .waitDurationInOpenState(Duration.ofSeconds(15))
-                            .permittedNumberOfCallsInHalfOpenState(3)
-                            .build())
-                    .timeLimiterConfig(TimeLimiterConfig.custom()
-                            .timeoutDuration(Duration.ofSeconds(2))
-                            .build()),
-                    "redisService");
-        };
-    }
+        @Bean
+        public Customizer<ReactiveResilience4JCircuitBreakerFactory> redisServiceCustomizer() {
+                return factory -> {
+                        factory.configure(builder -> builder
+                                        .circuitBreakerConfig(io.github.resilience4j.circuitbreaker.CircuitBreakerConfig
+                                                        .custom()
+                                                        .slidingWindowType(SlidingWindowType.COUNT_BASED)
+                                                        .slidingWindowSize(15)
+                                                        .failureRateThreshold(45)
+                                                        .waitDurationInOpenState(Duration.ofSeconds(15))
+                                                        .permittedNumberOfCallsInHalfOpenState(3)
+                                                        .build())
+                                        .timeLimiterConfig(TimeLimiterConfig.custom()
+                                                        .timeoutDuration(Duration.ofSeconds(2))
+                                                        .build()),
+                                        "redisService");
+                };
+        }
 
-    @Bean
-    public Customizer<ReactiveResilience4JCircuitBreakerFactory> defaultCustomizer() {
-        return factory -> {
-            factory.configureDefault(id -> new Resilience4JConfigBuilder(id)
-                    .circuitBreakerConfig(io.github.resilience4j.circuitbreaker.CircuitBreakerConfig.custom()
-                            .slidingWindowType(SlidingWindowType.COUNT_BASED)
-                            .slidingWindowSize(10)
-                            .failureRateThreshold(50)
-                            .waitDurationInOpenState(Duration.ofSeconds(10))
-                            .permittedNumberOfCallsInHalfOpenState(3)
-                            .build())
-                    .timeLimiterConfig(TimeLimiterConfig.custom()
-                            .timeoutDuration(Duration.ofSeconds(3))
-                            .build())
-                    .build());
-        };
-    }
+        @Bean
+        public Customizer<ReactiveResilience4JCircuitBreakerFactory> defaultCustomizer() {
+                return factory -> {
+                        factory.configureDefault(id -> new Resilience4JConfigBuilder(id)
+                                        .circuitBreakerConfig(io.github.resilience4j.circuitbreaker.CircuitBreakerConfig
+                                                        .custom()
+                                                        .slidingWindowType(SlidingWindowType.COUNT_BASED)
+                                                        .slidingWindowSize(10)
+                                                        .failureRateThreshold(50)
+                                                        .waitDurationInOpenState(Duration.ofSeconds(10))
+                                                        .permittedNumberOfCallsInHalfOpenState(3)
+                                                        .build())
+                                        .timeLimiterConfig(TimeLimiterConfig.custom()
+                                                        .timeoutDuration(Duration.ofSeconds(3))
+                                                        .build())
+                                        .build());
+                };
+        }
 }
--- admin-service/src/main/java/com/craftpilot/adminservice/config/MongoConfig.java
+++ admin-service/src/main/java/com/craftpilot/adminservice/config/MongoConfig.java
@@ -4,8 +4,7 @@ import com.mongodb.ConnectionString;
 import com.mongodb.MongoClientSettings;
 import com.mongodb.reactivestreams.client.MongoClient;
 import com.mongodb.reactivestreams.client.MongoClients;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -17,8 +16,8 @@ import java.util.concurrent.TimeUnit;
 
 @Configuration
 @EnableReactiveMongoRepositories(basePackages = "com.craftpilot.adminservice.repository")
+@Slf4j
 public class MongoConfig extends AbstractReactiveMongoConfiguration {
-    private static final Logger logger = LoggerFactory.getLogger(MongoConfig.class);
 
     @Value("${spring.data.mongodb.uri}")
     private String mongoUri;
@@ -26,14 +25,20 @@ public class MongoConfig extends AbstractReactiveMongoConfiguration {
     @Value("${spring.data.mongodb.database}")
     private String databaseName;
 
-    @Value("${spring.data.mongodb.connection-timeout:10000}")
-    private int connectionTimeout;
+    @Value("${spring.data.mongodb.connect-timeout:60000}")
+    private int connectTimeout;
 
-    @Value("${spring.data.mongodb.max-connection-idle-time:60000}")
+    @Value("${spring.data.mongodb.socket-timeout:60000}")
+    private int socketTimeout;
+
+    @Value("${spring.data.mongodb.max-wait-time:120000}")
+    private int maxWaitTime;
+
+    @Value("${spring.data.mongodb.max-connection-idle-time:300000}")
     private int maxConnectionIdleTime;
 
-    @Value("${spring.data.mongodb.connection-pool-max-size:50}")
-    private int connectionPoolMaxSize;
+    @Value("${spring.data.mongodb.max-connection-life-time:600000}")
+    private int maxConnectionLifeTime;
 
     @Override
     protected String getDatabaseName() {
@@ -43,96 +48,34 @@ public class MongoConfig extends AbstractReactiveMongoConfiguration {
     @Override
     @Bean
     public MongoClient reactiveMongoClient() {
-        try {
-            // Hassas bilgileri gizleyerek URI'yi loglama
-            String safeUri = mongoUri.replaceAll("mongodb://[^:]*:[^@]*@", "mongodb://***:***@");
-            logger.info("Initializing MongoDB configuration. Database: {}, URI: {}", databaseName, safeUri);
+        log.info("MongoDB yapılandırması başlatılıyor. Database: {}", databaseName);
 
-            // MongoDB bağlantı URI'sinde server adını çıkar ve logla
-            String serverAddress = extractServerAddress(mongoUri);
-            logger.info("Attempting to connect to MongoDB server at: {}", serverAddress);
+        // Hassas bilgileri gizleyerek URI'yi loglama
+        String safeUri = mongoUri.replaceAll("mongodb://[^:]*:[^@]*@", "mongodb://***:***@");
+        log.info("MongoDB URI (masked): {}", safeUri);
 
-            ConnectionString connectionString = new ConnectionString(mongoUri);
+        ConnectionString connectionString = new ConnectionString(mongoUri);
 
-            MongoClientSettings settings = MongoClientSettings.builder()
-                    .applyConnectionString(connectionString)
-                    .applyToConnectionPoolSettings(builder -> builder
-                            .maxSize(connectionPoolMaxSize)
-                            .minSize(5)
-                            .maxConnectionIdleTime(maxConnectionIdleTime, TimeUnit.MILLISECONDS)
-                            .maxWaitTime(15000, TimeUnit.MILLISECONDS))
-                    .applyToSocketSettings(builder -> builder
-                            .connectTimeout(connectionTimeout, TimeUnit.MILLISECONDS)
-                            .readTimeout(30000, TimeUnit.MILLISECONDS))
-                    .applyToServerSettings(builder -> builder
-                            .heartbeatFrequency(20000, TimeUnit.MILLISECONDS))
-                    .retryWrites(true)
-                    .retryReads(true)
-                    .build();
+        MongoClientSettings settings = MongoClientSettings.builder()
+                .applyConnectionString(connectionString)
+                .applyToConnectionPoolSettings(builder -> builder.maxSize(20)
+                        .minSize(5)
+                        .maxWaitTime(maxWaitTime, TimeUnit.MILLISECONDS)
+                        .maxConnectionIdleTime(maxConnectionIdleTime, TimeUnit.MILLISECONDS)
+                        .maxConnectionLifeTime(maxConnectionLifeTime, TimeUnit.MILLISECONDS))
+                .applyToSocketSettings(builder -> builder.connectTimeout(connectTimeout, TimeUnit.MILLISECONDS)
+                        .readTimeout(socketTimeout, TimeUnit.MILLISECONDS))
+                .applyToServerSettings(builder -> builder.heartbeatFrequency(20000, TimeUnit.MILLISECONDS))
+                .build();
 
-            logger.info("MongoDB configuration successfully initialized");
-            return MongoClients.create(settings);
-        } catch (Exception e) {
-            logger.error("Failed to configure MongoDB connection", e);
-            throw e;
-        }
+        log.info(
+                "MongoDB yapılandırması tamamlandı. Timeout ayarları: connectTimeout={}ms, socketTimeout={}ms, maxWaitTime={}ms",
+                connectTimeout, socketTimeout, maxWaitTime);
+        return MongoClients.create(settings);
     }
 
     @Bean
     public ReactiveMongoTemplate reactiveMongoTemplate() {
-        try {
-            return new ReactiveMongoTemplate(reactiveMongoClient(), getDatabaseName());
-        } catch (Exception e) {
-            logger.error("Failed to create ReactiveMongoTemplate", e);
-            throw e;
-        }
-    }
-
-    /**
-     * MongoDB URI'sinden server adresini çıkarır
-     */
-    private String extractServerAddress(String uri) {
-        try {
-            // mongodb://user:pass@server:port/db?options formatını parçala
-            int atIndex = uri.indexOf('@');
-            int slashAfterServerIndex = -1;
-
-            if (atIndex > 0) {
-                // @ işareti varsa ondan sonraki ilk / işaretine kadar al
-                slashAfterServerIndex = uri.indexOf('/', atIndex);
-                if (slashAfterServerIndex > 0) {
-                    return uri.substring(atIndex + 1, slashAfterServerIndex);
-                } else {
-                    // / işareti bulunamazsa, soru işaretine kadar al
-                    int questionIndex = uri.indexOf('?', atIndex);
-                    if (questionIndex > 0) {
-                        return uri.substring(atIndex + 1, questionIndex);
-                    } else {
-                        // soru işareti de bulunamazsa tümünü al
-                        return uri.substring(atIndex + 1);
-                    }
-                }
-            } else {
-                // @ işareti yoksa mongodb:// sonrası ilk / işaretine kadar al
-                int protocolIndex = uri.indexOf("://");
-                if (protocolIndex > 0) {
-                    slashAfterServerIndex = uri.indexOf('/', protocolIndex + 3);
-                    if (slashAfterServerIndex > 0) {
-                        return uri.substring(protocolIndex + 3, slashAfterServerIndex);
-                    } else {
-                        int questionIndex = uri.indexOf('?', protocolIndex);
-                        if (questionIndex > 0) {
-                            return uri.substring(protocolIndex + 3, questionIndex);
-                        } else {
-                            return uri.substring(protocolIndex + 3);
-                        }
-                    }
-                }
-            }
-            return "unknown-server";
-        } catch (Exception e) {
-            logger.warn("Failed to extract server address from MongoDB URI", e);
-            return "parsing-error";
-        }
+        return new ReactiveMongoTemplate(reactiveMongoClient(), getDatabaseName());
     }
 }
