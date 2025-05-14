312629659d9a5dabe6511d7b2df25582aa76f2b2
o-oada <adasunucum06@gmail.com>
2025-05-14 12:09:07 +0300

    feat: NotificationChannel sınıfı eklendi; bildirimlerin iletilebileceği çeşitli kanallar tanımlandı

---
+++ notification-service/src/main/java/com/craftpilot/notificationservice/model/NotificationChannel.java
@@ -0,0 +1,13 @@
+package com.craftpilot.notificationservice.model;
+
+/**
+ * Defines the various channels through which notifications can be delivered.
+ */
+public enum NotificationChannel {
+    EMAIL,
+    PUSH,
+    SMS,
+    WEB,
+    IN_APP,
+    WEBHOOK
+}
